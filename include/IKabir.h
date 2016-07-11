8a0f524997184cd399650d3fbd06406c09a3f8be
kabir <jxwazxzhp@126.com>
2016-07-11 15:12:54 +0800

    build error.

--- include/IKabir.h
+++ include/IKabir.h
@@ -24,7 +24,7 @@ public:
     virtual status_t onTransact(uint32_t code, const Parcel & data,
             Parcel * reply, uint32_t flags =
             0) = 0;
-;
+};
 // ---------------------------------------------------------------------------- 
 };				// namespace android
 
