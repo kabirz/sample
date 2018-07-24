2ac52f545706029ad51e75e5967a2c956f218015
Kabir <jxwazxzhp@126.com>
2018-07-24 14:30:07 +0800

    meson: add meson build system

--- build.sh
+++ build.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+USE_MESON=1
 set -e
 cd ${0%%$(basename $0)}
 rm build -fr
 mkdir build
 cd build
+if [ $USE_MESON -eq 1 ];then
+	meson ..
+else
+	cmake -GNinja -DCMAKE_BUILD_TYPE=DEBUG ..
+fi
 
-cmake -GNinja -DCMAKE_BUILD_TYPE=DEBUG .. && ninja
+if [ $? -eq 0 ]; then
+	ninja
+fi
